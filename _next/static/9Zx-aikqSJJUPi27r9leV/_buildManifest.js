41e209da711be5f04b3e8674d64fdb08413b5f7d
rizerphe <rizerphe@users.noreply.github.com>
2022-04-26 10:13:21 +0000

    deploy: 557d076c1a82484c1454b898960bbafee4207086

---
+++ _next/static/9Zx-aikqSJJUPi27r9leV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-89e14b955b01fe3b.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
