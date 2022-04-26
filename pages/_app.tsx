78427c786a600df0442218eddd57d35efd4a83a3
rizerphe <44440399+rizerphe@users.noreply.github.com>
2022-04-26 12:13:15 +0300

    Cleaned out the extra boilerplate
    Added a new favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
